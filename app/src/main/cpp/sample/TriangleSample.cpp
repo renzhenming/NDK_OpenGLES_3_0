d90e79cd1fdd8cf484f19df72fc17f16dd206a9b
renzhenming <1565766177@qq.com>
2023-11-14 08:02:31 +0800

    test add commit

--- app/src/main/cpp/sample/TriangleSample.cpp
+++ app/src/main/cpp/sample/TriangleSample.cpp
@@ -52,8 +52,8 @@ void TriangleSample::Draw(int screenW, int screenH)
 	LOGCATE("TriangleSample::Draw");
 	GLfloat vVertices[] = {
 			0.0f,  0.5f, 0.0f,
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
+			-1.0f, -0.5f, 0.0f,
+			1.0f, -0.5f, 0.0f,
 	};
 
 	if(m_ProgramObj == 0)
@@ -66,6 +66,13 @@ void TriangleSample::Draw(int screenW, int screenH)
 	glUseProgram (m_ProgramObj);
 
 	// Load the vertex data
+	//将当前的顶点属性与顶点缓冲对象（VBO）关联起来
+	//index 指定要配置的顶点属性的编号。
+	//size 指定每个顶点属性的分量数（1、2、3 或 4，就像向量的维度一样）。
+	//type 指定每个分量的数据类型，可以是 GL_BYTE、GL_UNSIGNED_BYTE、GL_SHORT、GL_UNSIGNED_SHORT、GL_INT、GL_UNSIGNED_INT、GL_FLOAT 或 GL_DOUBLE。
+	//normalized1 指定是否将数据归一化到 [0,1] 或 [-1,1] 范围内。
+	//stride （步长）指定连续两个顶点属性间的字节数。如果为 0，则表示顶点属性是紧密排列的。
+	//pointer 指向缓冲对象中第一个顶点属性的第一个分量的地址。（offset的作用）
 	glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, vVertices );
 	glEnableVertexAttribArray (0);
 
